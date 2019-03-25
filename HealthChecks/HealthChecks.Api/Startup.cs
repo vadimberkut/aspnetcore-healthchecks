ea169b6faf9851401c5435258c5cfc2112208404
vadimberkut <vadimberkut8@gmail.com>
2019-03-25 12:35:42 +0200

    setup

--- HealthChecks/HealthChecks.Api/Startup.cs
+++ HealthChecks/HealthChecks.Api/Startup.cs
@@ -1,15 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
 using System.Threading.Tasks;
+using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
 
 namespace HealthChecks.Api
 {
@@ -25,6 +31,14 @@ namespace HealthChecks.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHealthChecks()
+                .AddSqlServer(connectionString: "Data Source=.;Initial Catalog=sensee-old-project;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trusted_Connection=True;TrustServerCertificate=True;MultipleActiveResultSets=true", name: "mssql")
+                .AddRedis(redisConnectionString: "127.0.0.1:6379", name: "redis")
+                .AddAzureTableStorage(connectionString: "DefaultEndpointsProtocol=https;AccountName=FAKE;AccountKey=FAKE;EndpointSuffix=core.windows.net", name: "azure-tables")
+                .AddUrlGroup(uri: new Uri("http://testapi.com"), name: "test-api");
+
+            services.AddHealthChecksUI();
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
@@ -42,6 +56,33 @@ namespace HealthChecks.Api
             }
 
             app.UseHttpsRedirection();
+
+            app.UseHealthChecks("/hc",
+               new HealthCheckOptions
+               {
+                   ResponseWriter = async (context, report) =>
+                   {
+                       var result = JsonConvert.SerializeObject(
+                           new
+                           {
+                               status = report.Status.ToString(),
+                               errors = report.Entries.Select(e => new { key = e.Key, value = Enum.GetName(typeof(HealthStatus), e.Value.Status) })
+                           });
+                       context.Response.ContentType = MediaTypeNames.Application.Json;
+                       await context.Response.WriteAsync(result);
+                   }
+               });
+            app.UseHealthChecks("/healthz", new HealthCheckOptions()
+            {
+                Predicate = _ => true,
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
+
+            app.UseHealthChecksUI(setup => {
+                //setup.ApiPath = "/healthz";
+                setup.UIPath = "/healthcheckui";
+            });
+
             app.UseMvc();
         }
     }
